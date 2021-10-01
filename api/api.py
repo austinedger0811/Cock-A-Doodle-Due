7a63faf278829a36c33d01c8799281994f43e41f
Austin M. Edger <ame2194@columbia.edu>
2021-10-01 11:41:10 -0400

    Set up nav and theme provider

--- api/api.py
+++ api/api.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
 from firebase_admin.firestore import SERVER_TIMESTAMP
-import time
 
 app = Flask(__name__)
 CORS(app)
@@ -12,15 +11,40 @@ cred = credentials.Certificate('key.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+assignments = db.collection(u'assignments')
 
-assignments = db.collection('assignments')
+
+@app.route('/api/v1/assignments', methods=['GET'])
+def get_assignments():
+    return get_assignments_list()
 
 
-@app.route('/')
-def hello_world():
+@app.route('/api/v1/assignment/<id>', methods=['GET'])
+def get_assignment(id):
+    return jsonify(assignments.document(id).get().to_dict())
+
+
+@app.route('/api/v1/add-assignment', methods=['POST'])
+def add_assignmnets():
+    assignment = request.json
+    assignment_id = assignments.document().id
+    assignment['timestamp'] = SERVER_TIMESTAMP
+    assignment['id'] = assignment_id
+    assignments.document(assignment_id).set(assignment)
+    return get_assignments_list()
+
+
+@app.route('/api/v1/update-assignment-progress/<id>', methods=['PUT'])
+def update_assignment(id):
+    pass
+
+
+@app.route('/api/v1/delete-assignment/<id>', methods=['DELETE'])
+def delete_assignment(id):
+    assignments.document(id).delete()
+    return get_assignments_list()
+
+
+def get_assignments_list():
+    # TODO: add order_by() to assignments to order them properly
     return jsonify([doc.to_dict() for doc in assignments.stream()])
-
-
-@app.route('/api/v1/time')
-def get_current_time():
-    return {'time': time.time()}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ff6659',
+      main: '#d32f2f',
+      dark: '#9a0007',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#ff44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 const App = () => {
+
   return (
-    <div>Hello World.</div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Cock-A-Doodle-Due</Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Container>
+    </ThemeProvider>
   );
 }
+
+const Container = styled.div`
+  height: 100vh; 
+  background-color: gray;
+`
 
 export default App;
