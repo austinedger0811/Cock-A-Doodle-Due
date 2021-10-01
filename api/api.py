d07e7c6be0c4a1036f997ec61e3f8307a445b4d1
Austin M. Edger <ame2194@columbia.edu>
2021-10-01 10:28:42 -0400

    Connected to db

--- api/api.py
+++ api/api.py
@@ -1,9 +1,23 @@
+from flask import Flask, request, jsonify
+import firebase_admin
+from firebase_admin import credentials, firestore
 import time
-from flask import Flask
 
 app = Flask(__name__)
 
+cred = credentials.Certificate('key.json')
+firebase_admin.initialize_app(cred)
 
-@app.route('/time')
+db = firestore.client()
+
+assignments = db.collection('assignments')
+
+
+@app.route('/')
+def hello_world():
+    return jsonify([doc.to_dict() for doc in assignments.stream()])
+
+
+@app.route('/api/v1/time')
 def get_current_time():
     return {'time': time.time()}
