46941fbb11f2779e865687c1e648335b8594fe3d
Austin M. Edger <ame2194@columbia.edu>
2021-10-01 10:36:24 -0400

    added CORS

--- api/api.py
+++ api/api.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
+from firebase_admin.firestore import SERVER_TIMESTAMP
 import time
 
 app = Flask(__name__)
+CORS(app)
 
 cred = credentials.Certificate('key.json')
 firebase_admin.initialize_app(cred)
