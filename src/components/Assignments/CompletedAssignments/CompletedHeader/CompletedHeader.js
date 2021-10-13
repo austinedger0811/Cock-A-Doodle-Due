76050aa89129b551d8c81246ae7cdcaaf2030c7b
Austin M. Edger <ame2194@columbia.edu>
2021-10-13 11:48:52 -0400

    Added Clear All Button

--- src/components/Assignments/CompletedAssignments/CompletedHeader/CompletedHeader.js
+++ src/components/Assignments/CompletedAssignments/CompletedHeader/CompletedHeader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
 const CompletedHeader = ({ assignments, onAssignmentChange }) => {
@@ -9,9 +10,14 @@ const CompletedHeader = ({ assignments, onAssignmentChange }) => {
   const hours = Math.round(assignments.reduce((sum, assignment) => sum + assignment.estimate, 0) * 10) / 10
 
   return (
-    <Box mt={6} mb={2}>
-      <Typography variant='h5'>Completed</Typography> 
-      <Typography variant='body2'>{`${count} ${count > 1 ? "assignments" : "assignment"}, ${hours} ${hours > 1 ? "hours" : "hour"}`}</Typography>
+    <Box mt={6} mb={2} display="flex" justifyContent="space-between">
+      <Box>
+        <Typography variant='h5'>Completed</Typography> 
+        <Typography variant='body2'>{`${count} ${count > 1 ? "assignments" : "assignment"}, ${hours} ${hours > 1 ? "hours" : "hour"}`}</Typography>
+      </Box>
+      <Box>
+        <Button variant="outlined">Clear All</Button>
+      </Box>
     </Box> 
   )
 }
